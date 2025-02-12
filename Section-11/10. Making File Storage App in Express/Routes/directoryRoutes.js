31a3b0520425a5229cfe39e7d26418221b41656b
SouravDevs <sourav2x4x@gmail.com>
2025-02-13 00:26:31 +0530

    Correction done

--- Section-11/10. Making File Storage App in Express/Routes/directoryRoutes.js
+++ Section-11/10. Making File Storage App in Express/Routes/directoryRoutes.js
@@ -1,39 +1,34 @@
 import express from "express";
-import { mkdir, readdir, rename, rm, stat } from "fs/promises";
+import { mkdir, readdir, stat } from "fs/promises";
 import path from "path";
-import directoriesData from '../directoriesDB.json' with {type: 'json'}
-import filesData from "../filesDB.json" with {type: 'json'}
+import directoriesData from '../directoriesDB.json' with {type: "json"}
+import filesData from '../filesDB.json' with {type: "json"}
 
-
-const router = express.Router()
+const router = express.Router();
 
 // Read
 router.get("/:id?", async (req, res) => {
-  const {id} = req.params;
-  
-  
+  const {id} = req.params
   if(!id) {
     const directoryData = directoriesData[0]
-   const files =  directoryData.files.map((fileId) => 
+    const files = directoryData.files.map((fileId) => 
       filesData.find((file) => file.id === fileId)
     )
-
     res.json({...directoryData, files})
+  } else {
+    const directoryData = directoriesData.find((directory) => directory.id === id)
+    res.json(directoryData)
   }
-  else {
-   const directoryData = directoriesData.find((directory) => directory.id === id )
-  res.json(...directoryData)
- }
 });
 
-router.post('/?*', async (req, res) => {
-  const { 0: dirname} = req.params;
- try {
-  await mkdir(`./storage/${dirname}`)
-  res.json({message: "Directory Created"})
- } catch (err) {
-  res.json({error: err.message})
- }
-})
+router.post("/*", async (req, res) => {
+  const dirname = path.join("/", req.params[0]);
+  try {
+    await mkdir(`./storage/${dirname}`);
+    res.json({ message: "Directory Created!" });
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
 
-export default router
+export default router;
