676e82ab39567e080b40d51da362f37fd32cef37
SouravDevs <sourav2x4x@gmail.com>
2025-04-03 21:53:53 +0530

    36. Fixing ObjectId Validation in Our Storate App

--- Section-11/10. Making File Storage App in Express/server/middlewares/validateIdMiddleWare.js
+++ Section-11/10. Making File Storage App in Express/server/middlewares/validateIdMiddleWare.js
@@ -1,11 +1,8 @@
-export default function (req, res, next, id) {
-    console.log({ id });
-    console.log(req.method, 'from id');
+import { ObjectId } from "mongodb";
 
-    // const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  
-    if (false) {
-      return res.status(400).json({ error: `Invalid id: ${id}` })
-    }
-    next()
+export default function (req, res, next, id) {
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: `Invalid ID: ${id}` });
   }
+  next();
+}
--- Section-11/10. Making File Storage App in Express/server/routes/userRoutes.js
+++ Section-11/10. Making File Storage App in Express/server/routes/userRoutes.js
@@ -1,88 +1,71 @@
 import express from "express";
 import checkAuth from "../middlewares/authMiddleware.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-router.post('/register', async (req, res, next) => {
-  const {name, email, password} = req.body
+router.post("/register", async (req, res, next) => {
+  const { name, email, password } = req.body;
   const db = req.db;
-
+  const foundUser = await db.collection("users").findOne({ email });
+  if (foundUser) {
+    return res.status(409).json({
+      error: "User already exists",
+      message:
+        "A user with this email address already exists. Please try logging in or use a different email.",
+    });
+  }
   try {
-    const userCollection = db.collection("users");
+    const rootDirId = new ObjectId();
+    const userId = new ObjectId();
+
     const dirCollection = db.collection("directories");
+    const userRootDir = await dirCollection.insertOne({
+      _id: rootDirId,
+      name: `root-${email}`,
+      parentDirId: null,
+      userId
+    });
 
-    const foundUser = await userCollection.findOne({email, password});
-    console.log(foundUser);
-    if(foundUser) {
-      return res.status(409).json({
-        error: "User already exists",
-        message: "A user with this email address already exists. Please try logging in or use a different email."
-      })
-    }
-  
-  
-    // Create directory collection in storageApp
-    const userRootDir = await dirCollection.insertOne(
-      {
-        name: `root-${email}`,
-        parentDirId: null,
-      }
-    )
-  
-    const rootDirId = userRootDir.insertedId;
-  
-    const createdUser = await userCollection.insertOne(
-      {
-        name,
-        email,
-        password,
-        rootDirId
-      }
-    )
-  
-    const userId =  createdUser.insertedId;
-  
-    await dirCollection.updateOne(
-      {_id: rootDirId}, { $set: {userId}}
-    )
+    const createdUser = await db.collection("users").insertOne({
+      _id: userId,
+      name,
+      email,
+      password,
+      rootDirId,
+    });
 
-    res.status(201).json({message: "User Registered"})
-  } catch(err) {
-    next(err)
+
+    res.status(201).json({ message: "User Registered" });
+  } catch (err) {
+    next(err);
   }
+});
 
-})
-
-router.post('/login', async (req, res, next) => {
-  const {email, password} = req.body
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
   const db = req.db;
-
-  const userCollection = db.collection('users');
-  const user = await userCollection.findOne({email, password});
-  console.log(user);
-
-  if(!user) {
-    return res.status(404).json({error: 'Invalid Credentials'})
+  const user = await db.collection("users").findOne({ email, password });
+  if (!user) {
+    return res.status(404).json({ error: "Invalid Credentials" });
   }
-
-  res.cookie('uid', user._id.toString(), {
+  res.cookie("uid", user._id.toString(), {
     httpOnly: true,
-    maxAge: 60 * 1000 * 60 * 24 * 7
-  })
-  res.json({message: 'logged in'})
-})
+    maxAge: 60 * 1000 * 60 * 24 * 7,
+  });
+  res.json({ message: "logged in" });
+});
 
-
-router.get('/', checkAuth, (req, res) => {
+router.get("/", checkAuth, (req, res) => {
   res.status(200).json({
     name: req.user.name,
-    email: req.user.email
-  })
-})
+    email: req.user.email,
+  });
+});
 
-router.post('/logout', (req, res) => {
-  res.clearCookie('uid')
+router.post("/logout", (req, res) => {
+  res.clearCookie("uid");
+  res.status(204).end();
+});
 
-  res.status(200).json({message: 'Logged Out!'})
-})
 export default router;
