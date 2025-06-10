2d068fce75858d56eb22e56c31ae82f42eca55d4
SouravDevs <sourav2x4x@gmail.com>
2025-06-10 23:57:55 +0530

    27. Understanding Mongoose Object

--- Section-13/16. What are Virtuals in Mongoose/db.js
+++ Section-13/16. What are Virtuals in Mongoose/db.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://localhost:27017/");
-console.log("Database connection requested");
 
-console.log("Database connected");
 
-console.log("Running db.js");
+
+// await mongoose.connect("mongodb://localhost:27017/");
+
+// const db = mongoose.connection.db
+
+// const fruitsCollection = db.collection("fruits");
+
+// const result = await fruitsCollection.insertOne({name: "Mango"})
+
+// console.log(result);
+
+// await mongoose.disconnect()
