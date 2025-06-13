8d5bffe8ed0105f154253ff51a9d89972d5c73e9
SouravDevs <sourav2x4x@gmail.com>
2025-06-14 01:11:17 +0530

    sec-14-04. Trying to Fix Cookie Expiry Issue

--- Section-11/10. Making File Storage App in Express/server/controllers/userController.js
+++ Section-11/10. Making File Storage App in Express/server/controllers/userController.js
@@ -57,7 +57,7 @@ export const login = async (req, res, next) => {
   if (!user) {
     return res.status(404).json({ error: "Invalid Credentials" });
   }
-  res.cookie("uid", user._id.toString(), {
+  res.cookie("uid", user._id.toString() + Math.round((Date.now() / 1000 + 10)).toString(16), {
     httpOnly: true,
     maxAge: 60 * 1000 * 60 * 24 * 7,
   });
--- Section-11/10. Making File Storage App in Express/server/middlewares/authMiddleware.js
+++ Section-11/10. Making File Storage App in Express/server/middlewares/authMiddleware.js
@@ -6,7 +6,17 @@ export default async function checkAuth(req, res, next) {
   if (!uid) {
     return res.status(401).json({ error: "Not logged!" });
   }
-  const user = await db.collection("users").findOne({ _id: new ObjectId(uid) });
+
+  const id = uid.substr(0, 24)
+  const expiryTime = parseInt(uid.substr(24, 32), 16)
+  const currentTime = Math.round(Date.now() / 1000)
+
+  if(currentTime > expiryTime) {
+     res.clearCookie("uid");
+    return res.status(204).json({error: "Session expired"});
+  }
+
+  const user = await db.collection("users").findOne({ _id: new ObjectId(id) });
   if (!user) {
     return res.status(401).json({ error: "Not logged!" });
   }
